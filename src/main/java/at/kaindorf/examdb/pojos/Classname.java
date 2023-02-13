777892510829f311c63c843a1f972880b2d32780
Manuel Reicht <reichtmanuel01@gmail.com>
2023-02-13 11:59:03 +0100

    optimized

--- src/main/java/at/kaindorf/examdb/pojos/Classname.java
+++ src/main/java/at/kaindorf/examdb/pojos/Classname.java
@@ -7,7 +7,6 @@ import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
