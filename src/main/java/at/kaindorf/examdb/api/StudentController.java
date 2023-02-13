4311a310355ad8b6d460696b3f6b6e6e207c2456
Manuel Reicht <reichtmanuel01@gmail.com>
2023-02-13 12:43:29 +0100

    sorted Student Request

--- src/main/java/at/kaindorf/examdb/api/StudentController.java
+++ src/main/java/at/kaindorf/examdb/api/StudentController.java
@@ -1,18 +1,14 @@
 package at.kaindorf.examdb.api;
 
-import at.kaindorf.examdb.database.ClassnameRepository;
 import at.kaindorf.examdb.database.StudentRepository;
 import at.kaindorf.examdb.pojos.Student;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -31,6 +27,6 @@ public class StudentController {
             @PathVariable("classId") Long classId,
             @RequestParam(name="pageNo", defaultValue = "0") int pageNo) {
         Pageable page = PageRequest.of(pageNo, 10);
-        return ResponseEntity.of(Optional.of(studentRepo.findAllByClassname_ClassId(classId, page)));
+        return ResponseEntity.of(Optional.of(studentRepo.findAllByClassname_ClassIdOrderByLastnameAscFirstname(classId, page)));
     }
 }
--- src/main/java/at/kaindorf/examdb/database/StudentRepository.java
+++ src/main/java/at/kaindorf/examdb/database/StudentRepository.java
@@ -1,14 +1,10 @@
 package at.kaindorf.examdb.database;
 
-import at.kaindorf.examdb.pojos.Classname;
 import at.kaindorf.examdb.pojos.Student;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    List<Student> findAllByClassname_ClassId(Long classId);
-    Page<Student> findAllByClassname_ClassId(Long classId, Pageable pageable);
+    Page<Student> findAllByClassname_ClassIdOrderByLastnameAscFirstname(Long classId, Pageable pageable);
 }
